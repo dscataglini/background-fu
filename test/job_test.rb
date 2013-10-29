151078512209c8259299f38b3787b9f1a509c6ac
Diego Scataglini <dwebsubmit@gmail.com>
2013-10-29 12:38:17 -0400

    updated spec syntax

--- test/job_test.rb
+++ test/job_test.rb
@@ -2,13 +2,16 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 require File.dirname(__FILE__) + '/../lib/job.rb'
 
 class JobTest < Test::Unit::TestCase
-  
+
   def setup
     ActiveRecord::Base.allow_concurrency = false
     @job = Job.new
   end
-  
-  should_have_readonly_attributes :worker_class, :worker_method, :args
-  should_require_attributes :worker_class, :worker_method
-  
+
+  should have_readonly_attribute :worker_class
+  should have_readonly_attribute :worker_method
+  should have_readonly_attribute :args
+
+  should_validate_presence_of :worker_class, :worker_method
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,22 +2,22 @@
 $VERBOSE = false
 
 require 'rubygems'
-require 'mocha'
-require 'active_record'
 require 'test/unit'
+require 'mocha/setup'
+require 'active_record'
 
 # gem install thoughtbot-shoulda --source http://gems.github.com
 require 'shoulda'
 require 'shoulda/active_record'
 
 ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), 'test.log'))
-ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :dbfile  => ':memory:')
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
 
 ActiveRecord::Schema.define do
   create_table :jobs do |t|
     t.string  :worker_class
     t.string  :worker_method
-    
+
     t.text    :args
     t.text    :result
 
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define do
 
     t.integer :progress
     t.string  :state
-    
+
     t.integer :lock_version, :default => 0
-    
+
     t.timestamp :start_at
     t.timestamp :started_at
     t.timestamps
